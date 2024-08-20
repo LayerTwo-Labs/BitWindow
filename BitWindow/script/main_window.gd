2b2e4418baaad3be33d41c98e630e5fee592ddc2
CryptAxe <cryptaxe@gmail.com>
2024-08-20 08:58:41 -0700

    Basic main window tabs, overview layout, sc page layout

--- BitWindow/script/main_window.gd
+++ BitWindow/script/main_window.gd
@@ -6,4 +6,32 @@ func _ready() -> void:
 	
 	
 func _on_server_btc_new_block_count(height: int) -> void:
-	$LabelDebugBlockCount.text = str("Bitcoin block count: ", height)
+	$VBoxContainer/HBoxContainerBottomStatus/PanelContainer/LabelNumBlocks.text = str("Bitcoin block count: ", height)
+
+
+func _on_button_overview_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage.visible = false
+
+
+func _on_button_wallet_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/WalletPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/WalletPage.visible = false
+
+
+func _on_button_sidechains_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SidechainPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SidechainPage.visible = false
+
+
+func _on_button_settings_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = false
