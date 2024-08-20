2ae17f81ff11c2e8b8be34f833178ca7747df08b
CryptAxe <cryptaxe@gmail.com>
2024-08-20 10:45:28 -0700

    Check and display connection status of bitcoin, wallet, cusf client

--- BitWindow/script/main_window.gd
+++ BitWindow/script/main_window.gd
@@ -1,12 +1,75 @@
 extends Control
 
+const BITCOIN_UPDATE_DELAY : int = 10
+const WALLET_UPDATE_DELAY : int = 10
+const CUSF_UPDATE_DELAY : int = 10
+
+var connected_btc : bool = false
+var connected_wallet : bool = false
+var connected_cusf : bool = false
+
+var timer_bitcoin_update = null
+var timer_wallet_update = null
+var timer_cusf_update = null
+
 func _ready() -> void:
 	# For network code debugging, set the server node visible
 	$Server.visible = true
 	
+	# Create bitcoin update timer
+	timer_bitcoin_update = Timer.new()
+	add_child(timer_bitcoin_update)
+	timer_bitcoin_update.connect("timeout", update_bitcoin_data)
 	
+	timer_bitcoin_update.start(BITCOIN_UPDATE_DELAY)
+	
+	# Create wallet update timer
+	timer_wallet_update = Timer.new()
+	add_child(timer_wallet_update)
+	timer_wallet_update.connect("timeout", update_wallet_data)
+	
+	timer_wallet_update.start(WALLET_UPDATE_DELAY)
+	
+	# Create bitcoin update timer
+	timer_cusf_update = Timer.new()
+	add_child(timer_cusf_update)
+	timer_cusf_update.connect("timeout", update_cusf_data)
+	
+	timer_cusf_update.start(CUSF_UPDATE_DELAY)
+	
+	
+	call_deferred("display_connection_status")
+
+
 func _on_server_btc_new_block_count(height: int) -> void:
-	$VBoxContainer/HBoxContainerBottomStatus/PanelContainer/LabelNumBlocks.text = str("Bitcoin block count: ", height)
+	connected_btc = true
+	$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/LabelNumBlocks.text = str("Blocks: ", height)
+	display_connection_status()
+
+
+func _on_server_cusf_new_block_count(_height: int) -> void:
+	connected_cusf = true
+	display_connection_status()
+
+
+func _on_server_wallet_updated(_btc_balance: int) -> void:
+	connected_wallet = true
+	display_connection_status()
+	
+	
+func _on_server_btc_rpc_failed() -> void:
+	connected_btc = false
+	display_connection_status()
+	
+
+func _on_server_wallet_rpc_failed() -> void:
+	connected_wallet = false
+	display_connection_status()
+
+
+func _on_server_cusf_rpc_failed() -> void:
+	connected_cusf = false
+	display_connection_status()
 
 
 func _on_button_overview_toggled(toggled_on: bool) -> void:
@@ -35,3 +98,38 @@ func _on_button_settings_toggled(toggled_on: bool) -> void:
 		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = true
 	else:
 		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = false
+
+
+func display_connection_status() -> void:
+	if connected_btc:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectBitcoinNotConnected.visible = false
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectBitcoinConnected.visible = true
+	else:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectBitcoinNotConnected.visible = true
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectBitcoinConnected.visible = false
+
+	if connected_wallet:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectWalletNotConnected.visible = false
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectWalletConnected.visible = true
+	else:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectWalletNotConnected.visible = true
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectWalletConnected.visible = false
+		
+	if connected_cusf:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectCUSFNotConnected.visible = false
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectCUSFConnected.visible = true
+	else:
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectCUSFNotConnected.visible = true
+		$MarginContainer/VBoxContainer/PanelContainer/HBoxContainerBottomStatus/TextureRectCUSFConnected.visible = false
+
+
+func update_bitcoin_data() -> void:
+	$Server.rpc_bitcoin_getblockcount()
+
+
+func update_wallet_data() -> void:
+	$Server.rpc_wallet_getbalance()
+
+
+func update_cusf_data() -> void:
+	$Server.rpc_cusf_getblockcount()
--- BitWindow/script/server.gd
+++ BitWindow/script/server.gd
@@ -4,14 +4,28 @@ const RPC_USER : String = "abc"
 const RPC_PASS : String = "def"
 
 const BITCOIN_RPC_PORT : int = 8332
+const WALLET_RPC_PORT : int = -1 # TODO currently unknown
+const CUSF_RPC_PORT : int = -1   # TODO currently unknown
 
 @onready var http_rpc_btc_get_block_count: HTTPRequest = $RPCRequests/HTTPRPCBTCGetBlockCount
+@onready var http_rpc_wallet_get_balance: HTTPRequest = $RPCRequests/HTTPRPCWalletGetBalance
+@onready var http_rpc_cusf_get_block_count: HTTPRequest = $RPCRequests/HTTPRPCCUSFGetBlockCount
 
+# Signals that should be emitted regularly if connections are working
 signal btc_new_block_count(height : int)
+signal wallet_updated(btc_balance : int)
+signal cusf_new_block_count(height : int)
+
+# Signals that indicate connection failure to one of the backend softwares 
+signal btc_rpc_failed()
+signal wallet_rpc_failed()
+signal cusf_rpc_failed()
 
 
 func _on_button_test_connection_bitcoin_pressed() -> void:
 	rpc_bitcoin_getblockcount()
+	rpc_cusf_getblockcount()
+	rpc_wallet_getbalance()
 
 
 func make_rpc_request(port : int, method: String, params: Variant, http_request: HTTPRequest) -> void:
@@ -48,7 +62,15 @@ func rpc_bitcoin_getblockcount() -> void:
 	make_rpc_request(BITCOIN_RPC_PORT, "getblockcount", [], http_rpc_btc_get_block_count)
 
 
-func _on_httprpcbtc_get_block_count_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+func rpc_wallet_getbalance() -> void:
+	make_rpc_request(WALLET_RPC_PORT, "getbalance", [], http_rpc_wallet_get_balance)
+	
+	
+func rpc_cusf_getblockcount() -> void:
+	make_rpc_request(CUSF_RPC_PORT, "getblockcount", [], http_rpc_cusf_get_block_count)
+
+
+func _on_httprpcbtc_get_block_count_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	var res = parse_rpc_result(response_code, body)
 	var height : int = 0
 	if res.has("result"):
@@ -57,3 +79,28 @@ func _on_httprpcbtc_get_block_count_request_completed(result: int, response_code
 		btc_new_block_count.emit(height)
 	else:
 		print_debug("result error")
+		btc_rpc_failed.emit()
+
+
+func _on_httprpc_wallet_get_balance_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	var res = parse_rpc_result(response_code, body)
+	var balance : int = 0
+	if res.has("result"):
+		print_debug("Result: ", res.result)
+		balance = res.result
+		wallet_updated.emit(balance)
+	else:
+		print_debug("result error")
+		wallet_rpc_failed.emit()
+
+
+func _on_httprpccusf_get_block_count_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	var res = parse_rpc_result(response_code, body)
+	var height : int = 0
+	if res.has("result"):
+		print_debug("Result: ", res.result)
+		height = res.result
+		cusf_new_block_count.emit(height)
+	else:
+		print_debug("result error")
+		cusf_rpc_failed.emit()
